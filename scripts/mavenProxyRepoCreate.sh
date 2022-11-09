e8df484dfd479ecfbc9cfdb6552c2d60e1b468d3
amikshas <46197570+amikshas@users.noreply.github.com>
2022-11-09 10:15:21 +0530

    Create mavenProxyRepoCreate.sh

---
+++ scripts/mavenProxyRepoCreate.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -u <admin_username>:<admin_password> -d @mavenHostedProxyCreateTemplate.json 'https://nexusuat.finastra.com/nexus/service/rest/v1/repositories/maven/proxy'
